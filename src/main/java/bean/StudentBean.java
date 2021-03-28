696a3f3a5925bf49b6b9f04db4b2c727d3374862
Justin Carlitti <jcarlitti@Justins-MacBook-Pro.local>
2021-03-28 00:16:54 -0400

    Beginning stages of the StudentBean

--- src/main/java/bean/StudentBean.java
+++ src/main/java/bean/StudentBean.java
@@ -1,10 +1,10 @@
 /**
- * 
+ * This class is for the student Java Bean
  */
 package main.java.bean;
 
 /**
- * @author jinalshah
+ * @author Justin Carlitti
  *
  */
 public class StudentBean {
@@ -14,6 +14,28 @@ public class StudentBean {
 	 */
 	public StudentBean() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	private String studentId;
+	private String username;
+	private String streetAddress;
+	private String city;
+	private String state;
+	private String zipCode;
+	private String telephoneNum;
+	private String email;
+	private String url;
+	private String hsGradMonth;
+	private String hsGradYear;
+	private String likedMost;
+	private String interested;
+	private String comments;
+	private String recommend;
+	
+	
+	
+	public static void main(String[] args ) {
+		
 	}
 
 }
